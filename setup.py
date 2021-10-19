10b150ba03017ae7d089ad6eea85e9433f7c9958
Elias Hohl <elias.hohl@ehtec.co>
2021-10-18 20:28:02 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=[
-        cythonize("pie_chart_ocr.pyx", language_level=3),
-    ]
+    ext_modules=cythonize("pie_chart_ocr.pyx", language_level=3)
 )
 
