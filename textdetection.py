c31200da75df8dafd5f1e30b6299f4780d648a7b
Elias Hohl <elias.hohl@ehtec.co>
2021-10-31 17:30:26 -0400

    update gitignore

--- textdetection.py
+++ textdetection.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
+# taken from opencv_contrib. MIT license
+
 import sys
 import os
-
+from tqdm import tqdm
 import cv2 as cv
 import numpy as np
+
+SCALING_FACTOR = 2
 
 print('\ntextdetection.py')
 print('       A demo script of the Extremal Region Filter algorithm described in:')
@@ -17,6 +21,11 @@ if len(sys.argv) < 2:
 pathname = os.path.dirname(sys.argv[0])
 
 img = cv.imread(str(sys.argv[1]))
+
+img = cv.resize(img, (img.shape[1] * SCALING_FACTOR, img.shape[0] * SCALING_FACTOR), interpolation=cv.INTER_AREA)
+
+cv.imwrite('temp2/orig.png', img)
+
 # for visualization
 vis = img.copy()
 
@@ -31,14 +40,17 @@ for c in range(0,cn):
 # Apply the default cascade classifier to each independent channel (could be done in parallel)
 
 erc1 = cv.text.loadClassifierNM1('trained_classifierNM1.xml')
-er1 = cv.text.createERFilterNM1(erc1,16,0.00015,0.13,0.2,True,0.1)
+# er1 = cv.text.createERFilterNM1(erc1,6,0.00005,0.08,0.2,True,0.1)
+er1 = cv.text.createERFilterNM1(erc1,16,0.0000005,0.08,0.4,True,0.2)
+# er1 = cv.text.createERFilterNM1(erc1,1,0.00000015,0.13,0.005,False,0.1)
+# er1 = cv.text.createERFilterNM1(erc1,16,0.00015,0.13,0.2,True,0.1)
 
 erc2 = cv.text.loadClassifierNM2('trained_classifierNM2.xml')
 er2 = cv.text.createERFilterNM2(erc2,0.5)
 
 print("Extracting Class Specific Extremal Regions from "+str(len(channels))+" channels ...")
 print("    (...) this may take a while (...)")
-for channel in channels:
+for channel in tqdm(channels):
 
     regions = cv.text.detectRegions(channel,er1,er2)
 
@@ -51,5 +63,8 @@ for channel in channels:
         cv.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (255, 255, 255), 1)
 
 #Visualization
-cv.imshow("Text detection result", vis)
-cv.waitKey(0)
+# cv.imshow("Text detection result", vis)
+# cv.waitKey(0)
+
+cv.imwrite('temp2/img.png', vis)
+
