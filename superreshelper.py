26b6994371148a1763f5e79bf07c1f2956b3bad3
Elias Hohl <elias.hohl@ehtec.co>
2021-11-01 08:10:36 -0400

    add superreshelper.py

---
+++ superreshelper.py
@@ -0,0 +1,20 @@
+import cv2
+from cv2 import dnn_superres
+
+sr = dnn_superres.DnnSuperResImpl_create()
+
+image = cv2.imread('./pie_charts/image-024.jpg')
+
+path = "EDSR_x3.pb"
+
+sr.readModel(path)
+
+sr.setModel("edsr", 3)
+
+result = sr.upsample(image)
+
+# cv2.imshow(result)
+#
+# cv2.waitkey(0)
+
+cv2.imwrite('temp2/upscaled.png', result)
