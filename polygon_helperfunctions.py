46e3fa08a45bd8e4793684ad81b46fec4f79d973
Elias Hohl <elias.hohl@ehtec.co>
2021-11-01 15:34:13 -0400

    add polygon_helperfunctions.py

---
+++ polygon_helperfunctions.py
@@ -0,0 +1,46 @@
+import itertools
+from helperfunctions import group_pairs_to_nested_list, rect_from_pre
+from polygon_calc_wrapper import PolygonCalc
+
+
+# maximum distance of words to be recognized as belonging to the same word in terms of letter height
+MAX_WORD_DISTANCE_RATIO = 0.75
+
+
+# group ocr detected letters that belong to the same word together
+def group_words(filtered_res_tuples, max_word_distance_ratio=MAX_WORD_DISTANCE_RATIO):
+
+    L2 = []
+
+    comb = itertools.combinations(filtered_res_tuples, 2)
+
+    pc = PolygonCalc()
+
+    for elem in comb:
+
+        pre_p1 = tuple(elem[0][2:6])
+        pre_p2 = tuple(elem[1][2:6])
+
+        p1 = rect_from_pre(pre_p1)
+        p2 = rect_from_pre(pre_p2)
+
+        p1_height = pre_p1[3] - pre_p1[1]
+        p2_height = pre_p2[3] - pre_p2[1]
+
+        p_height = min(p1_height, p2_height)
+
+        max_word_dist = max_word_distance_ratio * p_height
+
+        min_dist = pc.min_poly_distance(p1, p2)
+
+        if min_dist < max_word_dist:
+            L2.append(elem)
+
+    del pc
+
+    for elem in filtered_res_tuples:
+        L2.append((elem, elem))
+
+    word_grouped_tuples = group_pairs_to_nested_list(L2)
+
+    return word_grouped_tuples
