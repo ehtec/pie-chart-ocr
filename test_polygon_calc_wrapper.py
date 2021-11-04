14ba9c887042d582d0517649a43052b6b9b933a2
Elias Hohl <elias.hohl@ehtec.co>
2021-11-04 19:09:52 -0400

    fix minor bug

--- test_polygon_calc_wrapper.py
+++ test_polygon_calc_wrapper.py
@@ -4,10 +4,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 from polygon_calc_wrapper import PolygonCalc
 
-pc = PolygonCalc
+pc = PolygonCalc()
 
 elements = [(1, 2, 3, 4),
             (5, 6, 7, 8),
             (9, 10, 11, 12)]
 
-print(pc.group_elements(elements))
+print(pc.group_elements(elements=elements))
