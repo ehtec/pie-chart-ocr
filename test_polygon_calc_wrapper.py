59a6a3b96e5f7a6320089f22004caa57cdd81ebe
Elias Hohl <elias.hohl@ehtec.co>
2021-11-04 21:20:54 -0400

    test with other threshold

--- test_polygon_calc_wrapper.py
+++ test_polygon_calc_wrapper.py
@@ -10,4 +10,4 @@ elements = [(1, 2, 3, 4),
             (5, 6, 7, 8),
             (9, 10, 11, 12)]
 
-print(pc.group_elements(elements=elements, threshold_dist=5.0))
+print(pc.group_elements(elements=elements, threshold_dist=1.0))
