00b100a3fb8046d803c6f2fa87b4eb96500c2183
Elias Hohl <elias.hohl@avl.com>
2021-11-15 19:56:25 +0100

    detect different shapes

--- shape_detection.py
+++ shape_detection.py
@@ -1,8 +1,23 @@
 import cv2
+import numpy as np
+import logging
+
+
+# max area ratio of a shape
+MAX_SHAPE_AREA_RATIO = 0.40
+
+# minimum absolute area of a shape in pixels
+MIN_SHAPE_AREA = 15
 
 
 # detect shapes in black-white RGB formatted cv2 image
 def detect_shapes(img):
+
+    shape = img.shape
+
+    height, width = shape[0], shape[1]
+
+    total_area = height * width
 
     # Morphological closing: get rid of holes
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
@@ -19,3 +34,47 @@ def detect_shapes(img):
     vis = cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
     cv2.imshow('vis', vis)
     cv2.waitKey(0)
+
+    for contour in contours:
+
+        area = cv2.contourArea(contour)
+
+        if area < MIN_SHAPE_AREA:
+            continue
+
+        if area > MAX_SHAPE_AREA_RATIO * total_area:
+            continue
+
+        # find the center of the shape
+
+        M = cv2.moments(contour)
+
+        if M['m00'] == 0.0:
+            logging.warning("Unable to compute shape center! Skipping.")
+            continue
+
+        x = int(M['m10'] / M['m00'])
+        y = int(M['m01'] / M['m00'])
+
+        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+
+        la = len(approx)
+
+        if la < 3:
+            logging.warning("Invalid shape detected! Skipping.")
+            continue
+
+        if la == 3:
+            logging.info("Triangle detected at position {0}".format((x, y)))
+
+        elif la == 4:
+            logging.info("Quadrilateral detected at position {0}".format((x, y)))
+
+        elif la == 5:
+            logging.info("Pentagon detected at position {0}".format((x, y)))
+
+        elif la == 6:
+            logging.info("Hexagon detected at position {0}".format((x, y)))
+
+        else:
+            logging.info("Circle, ellipse or arbitrary shape detected at position {0}".format((x, y)))
