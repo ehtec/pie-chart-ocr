5b4ddb54feef5a79d46247f8b3f3f6327fb9f1ec
Elias Hohl <elias.hohl@avl.com>
2021-11-26 14:50:44 +0100

    edit check_ellipse_or_circle function

--- shape_detection.py
+++ shape_detection.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import logging
 from ellipse import LsqEllipse
+import matplotlib.pyplot as plt
+from polygon_calc_wrapper import PolygonCalc
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -43,6 +46,28 @@ def check_ellipse_or_circle(arr, max_deviation=MAX_DEVIATION):
     logging.info("Ellipse parameters: {0}".format(reg_params))
 
     center, width, height, phi = reg_params
+
+    t_values = np.linspace(0.0, 2 * np.pi, 1000)
+
+    x_values = (width * np.cos(t_values) * np.cos(phi) - height * np.sin(t_values) * np.sin(phi)) / 2
+
+    y_values = (width * np.cos(t_values) * np.sin(phi) - height * np.sin(t_values) * np.cos(phi)) / 2
+
+    res_arr = np.column_stack([x_values, y_values])
+
+    plt.plot(arr[:, 0], arr[:, 1], 'bo')
+    plt.plot(x_values, y_values, 'r-')
+    plt.show()
+
+    pc = PolygonCalc()
+
+    intersection_area = pc.poly_intersection_area(arr.tolist(), res_arr.tolist())
+
+    total_area = cv2.contourArea(arr.tolist()) + cv2.contourArea(res_arr.tolist())
+
+    area_deviation_ratio = 2 * (total_area - 2 * intersection_area) / total_area
+
+    logging.info("area_deviation_ratio: {0}".format(area_deviation_ratio))
 
 
 # check if an array of four points and shape (4, 2) is a square (returning 2), a rectangle (returning 1) or neither
