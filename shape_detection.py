a92b5fc6775a6948a55d19499ff3f1965d5e91c9
Elias Hohl <elias.hohl@ehtec.co>
2021-12-16 14:15:30 +0100

    add debug messages

--- shape_detection.py
+++ shape_detection.py
@@ -193,7 +193,8 @@ def detect_shapes(img, approx_poly_accuracy=APPROX_POLY_ACCURACY):
     logging.info("len(contours): {0}".format(len(contours)))
     logging.info("len(hierarchy): {0}".format(len(hierarchy)))
     logging.info("contours.shape: {0}".format(contours.shape))
-    logging.info("hierarchy.shape: {0}".format(hierarchy.shape))
+    # logging.info("hierarchy.shape: {0}".format(hierarchy.shape))
+    pprint(hierarchy)
 
     logging.info("Number of found contours for shape detection: {0}".format(len(contours)))
 
