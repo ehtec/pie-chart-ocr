65ead718f3e455258f47406c51f286c7c8903233
Elias Hohl <elias.hohl@ehtec.co>
2021-12-16 21:56:31 +0100

    add ellipse test

--- test_ellipse_detection.py
+++ test_ellipse_detection.py
@@ -1,9 +1,19 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+#
+# from skimage import data
+# import ellipse_detection
+#
+# # Load picture, convert to grayscale and detect edges
+# image_rgb = data.coffee()[0:220, 160:420]
+#
+# ellipse_detection.detect_ellipses(image_rgb)
 
-from skimage import data
-import ellipse_detection
+from ellipse_example import make_test_ellipse
+import numpy as np
+import shape_detection
 
-# Load picture, convert to grayscale and detect edges
-image_rgb = data.coffee()[0:220, 160:420]
+X1, X2 = make_test_ellipse()
 
-ellipse_detection.detect_ellipses(image_rgb)
+X = np.array(list(zip(X1, X2)))
+
+shape_detection.check_ellipse_or_circle(X)
