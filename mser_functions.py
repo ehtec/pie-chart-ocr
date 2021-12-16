95abc438964c923e159160cb2f8070944e0c6c57
Elias Hohl <elias.hohl@ehtec.co>
2021-12-16 19:15:59 +0100

    test with different background color distance

--- mser_functions.py
+++ mser_functions.py
@@ -53,7 +53,7 @@ BORDER_WIDTH = 15
 OUTER_BORDER_WIDTH = 22
 
 # color distance difference (CIE2000) for background color check
-BG_COLOR_DISTANCE = 18  # 13.0
+BG_COLOR_DISTANCE = 15  # 13.0
 
 # set minimum confidence threshold
 MIN_CONFIDENCE = 20
