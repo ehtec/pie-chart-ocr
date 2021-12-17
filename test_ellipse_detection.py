7681dffab49c99d1ff0784dd0fa541fd6f57d0f1
Elias Hohl <elias.hohl@ehtec.co>
2021-12-17 17:35:55 +0100

    move order_edges function

--- test_ellipse_detection.py
+++ test_ellipse_detection.py
@@ -14,39 +14,7 @@ import shape_detection
 from pprint import pprint
 import matplotlib.pyplot as plt
 # import alphashape
-from test_alphashape_2 import alpha_shape
-
-
-def order_edges(unordered_edges):
-
-    edges_l = list(zip(*list(unordered_edges)))
-
-    if len(edges_l[0]) != len(list(set(edges_l[0]))):
-        raise ValueError("Duplicate point A found!")
-
-    if len(edges_l[1]) != len(list(set(edges_l[1]))):
-        raise ValueError("Duplicate point B found!")
-
-    ordered_edges = [unordered_edges[0]]
-
-    while len(ordered_edges) < len(unordered_edges):
-
-        # print("ordered_edges: {0}".format(ordered_edges))
-
-        found_edge = False
-
-        for el in unordered_edges:
-            # print("el: {0}".format(el))
-            if el[0] == ordered_edges[-1][1]:
-                ordered_edges.append(el)
-                found_edge = True
-                break
-
-        if not found_edge:
-            print("len(ordered_edges): {0}".format(len(ordered_edges)))
-            raise ValueError("This should not be reached.")
-
-    return ordered_edges
+from hull_computation import order_edges, alpha_shape
 
 
 X1, X2 = make_test_ellipse()
