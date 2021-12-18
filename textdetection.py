e42a7a992734b158ec60ee1ef492a1b1cc42c655
Elias Hohl <elias.hohl@ehtec.co>
2021-12-18 02:24:02 +0100

    fix format

--- textdetection.py
+++ textdetection.py
@@ -6,7 +6,7 @@ import sys
 import os
 from tqdm import tqdm
 import cv2 as cv
-import numpy as np
+# import numpy as np
 
 SCALING_FACTOR = 2
 
@@ -33,38 +33,37 @@ vis = img.copy()
 # Extract channels to be processed individually
 channels = list(cv.text.computeNMChannels(img))
 # Append negative channels to detect ER- (bright regions over dark background)
-cn = len(channels)-1
-for c in range(0,cn):
-    channels.append(255-channels[c])
+cn = len(channels) - 1
+for c in range(0, cn):
+    channels.append(255 - channels[c])
 
 # Apply the default cascade classifier to each independent channel (could be done in parallel)
 
 erc1 = cv.text.loadClassifierNM1('trained_classifierNM1.xml')
 # er1 = cv.text.createERFilterNM1(erc1,6,0.00005,0.08,0.2,True,0.1)
-er1 = cv.text.createERFilterNM1(erc1,16,0.0000005,0.08,0.4,True,0.2)
+er1 = cv.text.createERFilterNM1(erc1, 16, 0.0000005, 0.08, 0.4, True, 0.2)
 # er1 = cv.text.createERFilterNM1(erc1,1,0.00000015,0.13,0.005,False,0.1)
 # er1 = cv.text.createERFilterNM1(erc1,16,0.00015,0.13,0.2,True,0.1)
 
 erc2 = cv.text.loadClassifierNM2('trained_classifierNM2.xml')
-er2 = cv.text.createERFilterNM2(erc2,0.5)
+er2 = cv.text.createERFilterNM2(erc2, 0.5)
 
-print("Extracting Class Specific Extremal Regions from "+str(len(channels))+" channels ...")
+print("Extracting Class Specific Extremal Regions from " + str(len(channels)) + " channels ...")
 print("    (...) this may take a while (...)")
 for channel in tqdm(channels):
 
-    regions = cv.text.detectRegions(channel,er1,er2)
+    regions = cv.text.detectRegions(channel, er1, er2)
 
-    rects = cv.text.erGrouping(img,channel,[r.tolist() for r in regions])
-    #rects = cv.text.erGrouping(img,channel,[x.tolist() for x in regions], cv.text.ERGROUPING_ORIENTATION_ANY,'../../GSoC2014/opencv_contrib/modules/text/samples/trained_classifier_erGrouping.xml',0.5)
+    rects = cv.text.erGrouping(img, channel, [r.tolist() for r in regions])
+    # rects = cv.text.erGrouping(img,channel,[x.tolist() for x in regions], cv.text.ERGROUPING_ORIENTATION_ANY,'../../GSoC2014/opencv_contrib/modules/text/samples/trained_classifier_erGrouping.xml',0.5)
 
-    #Visualization
+    # Visualization
     for rect in rects:
-        cv.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (0, 0, 0), 2)
-        cv.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (255, 255, 255), 1)
+        cv.rectangle(vis, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 0, 0), 2)
+        cv.rectangle(vis, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (255, 255, 255), 1)
 
-#Visualization
+# Visualization
 # cv.imshow("Text detection result", vis)
 # cv.waitKey(0)
 
 cv.imwrite('temp2/img.png', vis)
-
