99a32172495d209faccdfd2aed632b2688a95538
Elias Hohl <elias.hohl@ehtec.co>
2021-12-23 14:54:42 -0500

    test cluster_rel_1d function

--- test_helperfunctions.py
+++ test_helperfunctions.py
@@ -6,5 +6,7 @@ import helperfunctions
 input_values = [0, 1, 2, 60, 3, 59, 62, 61, 79, -5]
 
 abs_clusters = helperfunctions.cluster_abs_1d(input_values, 1.0)
+rel_clusters = helperfunctions.cluster_abs_1d(input_values, 0.05)
 
 logging.info("abs_clusters: {0}".format(abs_clusters))
+logging.info("rel_clusters: {0}".format(rel_clusters))
