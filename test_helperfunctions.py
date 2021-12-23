94332ff94a95f6d3548952766f4f28b24c92c773
Elias Hohl <elias.hohl@ehtec.co>
2021-12-23 18:08:23 -0500

    test cluster_dbscan function

--- test_helperfunctions.py
+++ test_helperfunctions.py
@@ -1,6 +1,7 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 import helperfunctions
+import numpy as np
 
 
 input_values = [0, 1, 2, 60, 3, 59, 62, 61, 79, -5, -100, -101, -103]
@@ -10,3 +11,9 @@ rel_clusters = helperfunctions.cluster_rel_1d(input_values, 0.05)
 
 logging.info("abs_clusters: {0}".format(abs_clusters))
 logging.info("rel_clusters: {0}".format(rel_clusters))
+
+data = np.random.rand(20, 2)
+
+dbscan_clusters = helperfunctions.cluster_dbscan(data, 0.12)
+
+logging.info("dbscan_clusters: {0}".format(dbscan_clusters))
