c21c037cb9e89f7551070757623f8136c5b67723
Elias Hohl <elias.hohl@ehtec.co>
2021-12-23 15:04:28 -0500

    more testing

--- test_helperfunctions.py
+++ test_helperfunctions.py
@@ -3,7 +3,7 @@ logging.basicConfig(level=logging.DEBUG)
 import helperfunctions
 
 
-input_values = [0, 1, 2, 60, 3, 59, 62, 61, 79, -5]
+input_values = [0, 1, 2, 60, 3, 59, 62, 61, 79, -5, -100, -101, -103]
 
 abs_clusters = helperfunctions.cluster_abs_1d(input_values, 1.0)
 rel_clusters = helperfunctions.cluster_rel_1d(input_values, 0.05)
