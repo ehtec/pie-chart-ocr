32cc7bc8bb84e21b75399026b8cf357435b7dd37
Elias Hohl <elias.hohl@ehtec.co>
2021-12-24 11:20:56 -0500

    use main function

--- examples/test_ellipse_detection.py
+++ examples/test_ellipse_detection.py
@@ -8,85 +8,91 @@
 #
 # ellipse_detection.detect_ellipses(image_rgb)
 
-from ellipse_example import make_test_ellipse
+from piechartocr.ellipse_example import make_test_ellipse
 import numpy as np
-import shape_detection
+from piechartocr import shape_detection
 from pprint import pprint
 import matplotlib.pyplot as plt
 # import alphashape
-from hull_computation import order_edges, alpha_shape, edges_to_contour, concave_hull
+from piechartocr.hull_computation import order_edges, alpha_shape, edges_to_contour, concave_hull
 import cv2
 from shapely.geometry import Polygon
 
 
-X1, X2 = make_test_ellipse()
+def main():
 
-points = np.array(list(zip(X1, X2)))
+    X1, X2 = make_test_ellipse()
 
-# alpha = 0.95 * alphashape.optimizealpha(points)
-#
-# X = alphashape.alphashape(points, alpha)
+    points = np.array(list(zip(X1, X2)))
 
-edges = alpha_shape(points, alpha=0.1, only_outer=True)
+    # alpha = 0.95 * alphashape.optimizealpha(points)
+    #
+    # X = alphashape.alphashape(points, alpha)
 
-X = points
+    edges = alpha_shape(points, alpha=0.1, only_outer=True)
 
-print("cv2.contourArea(X): {0}".format(cv2.contourArea(X.astype(np.int64))))
+    X = points
 
-# print("X.shape: {0}".format(X.shape))
-# print("X:")
-# # pprint(edges)
-#
-# edges_list = list(zip(*list(edges)))
-#
-# print("once: {0}".format([el for el in edges if el[1] not in edges_list[0]]))
-#
-# edges = order_edges(list(edges))
-#
-# # plt.plot(X[:, 0], X[:, 1], 'r-')
-# # plt.plot(points[:, 0], points[:, 1], '.')
-# # for i, j in edges:
-# #     plt.plot(X[:, 0], X[:, 1], 'r-')
-# #     plt.plot(points[[i, j], 0], points[[i, j], 1])
-# #     plt.show()
-#
-# contour = edges_to_contour(points, edges)
+    print("cv2.contourArea(X): {0}".format(cv2.contourArea(X.astype(np.int64))))
 
-contour = concave_hull(points)
+    # print("X.shape: {0}".format(X.shape))
+    # print("X:")
+    # # pprint(edges)
+    #
+    # edges_list = list(zip(*list(edges)))
+    #
+    # print("once: {0}".format([el for el in edges if el[1] not in edges_list[0]]))
+    #
+    # edges = order_edges(list(edges))
+    #
+    # # plt.plot(X[:, 0], X[:, 1], 'r-')
+    # # plt.plot(points[:, 0], points[:, 1], '.')
+    # # for i, j in edges:
+    # #     plt.plot(X[:, 0], X[:, 1], 'r-')
+    # #     plt.plot(points[[i, j], 0], points[[i, j], 1])
+    # #     plt.show()
+    #
+    # contour = edges_to_contour(points, edges)
 
-# contour = concave_hull(contour)
-#
-# contour = concave_hull(contour)
-#
-# contour = concave_hull(contour)
-#
-# contour = concave_hull(contour)
-#
-# contour = concave_hull(contour)
-#
-# contour = concave_hull(contour)
+    contour = concave_hull(points)
 
-plt.plot(contour[:, 0], contour[:, 1], 'r-')
-plt.show()
+    # contour = concave_hull(contour)
+    #
+    # contour = concave_hull(contour)
+    #
+    # contour = concave_hull(contour)
+    #
+    # contour = concave_hull(contour)
+    #
+    # contour = concave_hull(contour)
+    #
+    # contour = concave_hull(contour)
 
-# X = concave_hull(X)
+    plt.plot(contour[:, 0], contour[:, 1], 'r-')
+    plt.show()
 
-# X = X.astype(np.float64)
+    # X = concave_hull(X)
 
-# X = X * 1000
+    # X = X.astype(np.float64)
 
-# X = X.astype(np.int64)
+    # X = X * 1000
 
-# concave_hull(X)
+    # X = X.astype(np.int64)
 
-poly1 = Polygon(X.tolist())
-print("poly1.is_valid: {0}".format(poly1.is_valid))
+    # concave_hull(X)
 
-# print("X:")
-# pprint(X.tolist())
+    poly1 = Polygon(X.tolist())
+    print("poly1.is_valid: {0}".format(poly1.is_valid))
 
-print("cv2.contourArea(X): {0}".format(cv2.contourArea(X.astype(np.int64))))
+    # print("X:")
+    # pprint(X.tolist())
 
-shape_detection.get_area_deviation_ratio(X, X)
+    print("cv2.contourArea(X): {0}".format(cv2.contourArea(X.astype(np.int64))))
 
-# shape_detection.check_ellipse_or_circle(X)
+    shape_detection.get_area_deviation_ratio(X, X)
+
+    # shape_detection.check_ellipse_or_circle(X)
+
+
+if __name__ == "__main__":
+    main()
