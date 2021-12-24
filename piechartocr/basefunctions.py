eb84215692d584cd713bbf88d6438ba9a0593a36
Elias Hohl <elias.hohl@ehtec.co>
2021-12-24 18:16:40 -0500

    add find_lib function

--- piechartocr/basefunctions.py
+++ piechartocr/basefunctions.py
@@ -1,3 +1,8 @@
+import logging
+import os
+import platform
+
+
 # convert complex to real, throw error if imaginary part is not 0. Return orignial number if it is not a complex.
 def complex_to_real(c):
 
@@ -7,3 +12,33 @@ def complex_to_real(c):
     assert c.imag == 0
 
     return c.real
+
+
+# find lib by keyword in search path
+def find_lib(search_path, keyword):
+
+    if not os.path.isdir(search_path):
+        logging.warning("Path {0} is not a directory".format(search_path))
+        return None
+
+    files = os.listdir(search_path)
+
+    if platform.system().upper() == "WINDOWS":
+        fileext = ".dll"
+
+    else:
+        fileext = ".so"
+
+    files = [el for el in files if el.lower().endswith(fileext)]
+
+    files = [el for el in files if keyword in el]
+
+    if not bool(files):
+        return None
+
+    if len(files) > 1:
+        logging.warning("Multiple matches found: {0}".format(files))
+
+    logging.info("Matching library found: {0}".format(files[0]))
+
+    return files[0]
