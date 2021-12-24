8ccfd8ebfaf4e5948763af33079357cb617c6c6f
Elias Hohl <elias.hohl@ehtec.co>
2021-12-24 18:37:12 -0500

    move files to old

--- old/run_examples.py
+++ old/run_examples.py
@@ -24,6 +24,6 @@ from examples import test_polygon_calc_wrapper, test_superreshelper, test_pie_ch
 # test_helperfunctions.main()
 # test_ellipse_detection.main()
 # test_mser_functions.main()
-# test_polygon_calc_wrapper.main()
+test_polygon_calc_wrapper.main()
 # test_superreshelper.main()
-test_pie_chart_ocr.main()
+# test_pie_chart_ocr.main()
