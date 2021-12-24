31d733aa371dbb461e60723be85c20673d2b8f72
Elias Hohl <elias.hohl@ehtec.co>
2021-12-24 11:16:40 -0500

    use main function

--- examples/test_helperfunctions.py
+++ examples/test_helperfunctions.py
@@ -4,16 +4,22 @@ from piechartocr import helperfunctions
 import numpy as np
 
 
-input_values = [0, 1, 2, 60, 3, 59, 62, 61, 79, -5, -100, -101, -103]
+def main():
 
-abs_clusters = helperfunctions.cluster_abs_1d(input_values, 1.0)
-rel_clusters = helperfunctions.cluster_rel_1d(input_values, 0.05)
+    input_values = [0, 1, 2, 60, 3, 59, 62, 61, 79, -5, -100, -101, -103]
 
-logging.info("abs_clusters: {0}".format(abs_clusters))
-logging.info("rel_clusters: {0}".format(rel_clusters))
+    abs_clusters = helperfunctions.cluster_abs_1d(input_values, 1.0)
+    rel_clusters = helperfunctions.cluster_rel_1d(input_values, 0.05)
 
-data = np.random.rand(20, 2)
+    logging.info("abs_clusters: {0}".format(abs_clusters))
+    logging.info("rel_clusters: {0}".format(rel_clusters))
 
-dbscan_clusters = helperfunctions.cluster_dbscan(data, 0.12)
+    data = np.random.rand(20, 2)
 
-logging.info("dbscan_clusters: {0}".format(dbscan_clusters))
+    dbscan_clusters = helperfunctions.cluster_dbscan(data, 0.12)
+
+    logging.info("dbscan_clusters: {0}".format(dbscan_clusters))
+
+
+if __name__ == "__main__":
+    main()
