d969f9e85db847990e7821df5ce32a1c1babfd2e
Elias Hohl <elias.hohl@ehtec.co>
2021-12-24 11:36:44 -0500

    add slov_ratio parameter

--- examples/test_polygon_calc_wrapper.py
+++ examples/test_polygon_calc_wrapper.py
@@ -13,7 +13,7 @@ def main():
                 (5, 6, 7, 8),
                 (9, 10, 11, 12)]
 
-    print(pc.group_elements(elements=elements, threshold_dist=0.25))
+    print(pc.group_elements(elements=elements, threshold_dist=0.25, slov_ratio=0.92))
 
 
 if __name__ == "__main__":
