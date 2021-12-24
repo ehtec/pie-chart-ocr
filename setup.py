2cf8c59cb587c9ef6bf662dfedfed521445fb78d
Elias Hohl <elias.hohl@ehtec.co>
2021-12-24 16:34:09 -0500

    also compile polygoncalc module

--- setup.py
+++ setup.py
@@ -10,7 +10,16 @@ colorprocesser_module = Extension(
     library_dirs=['/usr/local/lib']
 )
 
+polygoncalc_module = Extension(
+    'libpolygoncalc',
+    sources=['src/polygoncalc.cpp'],
+    define_macros=[('MAJOR_VERSION', '0'), ('MINOR_VERSION', 3)],
+    include_dirs=[],
+    libraries=['boost_system'],
+    library_dirs=['/usr/local/lib']
+)
+
 setup(name="PieChartOCR",
       version="0.3",
       description="Pie Chart Optical Character Recognition",
-      ext_modules=[colorprocesser_module])
+      ext_modules=[colorprocesser_module, polygoncalc_module])
