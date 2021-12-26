c3e32246d7af82ea0be6d543a41428ba3d09f647
Elias Hohl <elias.hohl@ehtec.co>
2021-12-25 19:49:05 -0500

    add runfiles

---
+++ run_pie_chart_ocr.py
@@ -0,0 +1,6 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from examples import test_pie_chart_ocr
+
+
+test_pie_chart_ocr.main()
---
+++ run_superreshelper.py
@@ -0,0 +1,6 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from examples import test_superreshelper
+
+
+test_superreshelper.main()
