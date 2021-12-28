6478e2797035ee267b386ece115a9e3250069710
Elias Hohl <elias.hohl@ehtec.co>
2021-12-28 14:32:35 -0500

    add compare_test_metrics function

--- piechartocr/metrics.py
+++ piechartocr/metrics.py
@@ -17,3 +17,28 @@ def load_test_metrics_json():
     logging.debug("test_metrics: {0}".format(test_metrics))
 
     return test_metrics
+
+
+# compare loaded test metrics to existing data files
+def compare_test_metrics(error_on_diff=True, error_on_miss=True):
+
+    test_metrics = load_test_metrics_json()
+
+    n_list = test_data_percentages()
+    n_list = [str(n) for n in n_list]
+
+    missing_metrics = [el for el in n_list if el not in test_metrics.keys()]
+    logging.info("missing_metrics: {0}".format(missing_metrics))
+
+    additional_metrics = [el for el in test_metrics.keys() if el not in n_list]
+    logging.info("additional_metrics: {0}".format(additional_metrics))
+
+    if error_on_diff:
+        if bool(missing_metrics) or bool(additional_metrics):
+            raise ValueError("Metrics do not match")
+
+    elif error_on_miss:
+        if bool(missing_metrics):
+            raise ValueError("Some metrics are missing")
+
+    return missing_metrics, additional_metrics
