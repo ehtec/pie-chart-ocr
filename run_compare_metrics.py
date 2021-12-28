13c15e27d725a496427b777a587cd0d2fb812ca0
Elias Hohl <elias.hohl@ehtec.co>
2021-12-28 17:12:11 -0500

    add run_compare_metrics.py

---
+++ run_compare_metrics.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from piechartocr.metrics import compare_test_metrics, load_test_metrics_json, compute_metrics
+
+
+test_metrics = load_test_metrics_json()
+
+compare_test_metrics(error_on_diff=False, error_on_miss=False, test_metrics=test_metrics)
