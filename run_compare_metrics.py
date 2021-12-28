2a419d71cd0ac054262b2dd3ae273b714b150240
Elias Hohl <elias.hohl@ehtec.co>
2021-12-28 17:53:06 -0500

    remove unused pipeline

--- run_compare_metrics.py
+++ run_compare_metrics.py
@@ -1,6 +1,6 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
-from piechartocr.metrics import compare_test_metrics, load_test_metrics_json, compute_metrics
+from piechartocr.metrics import compare_test_metrics, load_test_metrics_json
 
 
 test_metrics = load_test_metrics_json()
