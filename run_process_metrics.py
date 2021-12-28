ef1a882b907681bd88d3b05d71b8cbc3924b944f
Elias Hohl <elias.hohl@ehtec.co>
2021-12-28 16:20:18 -0500

    test compute_metrics function

--- run_process_metrics.py
+++ run_process_metrics.py
@@ -1,6 +1,10 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
-from piechartocr.metrics import compare_test_metrics
+from piechartocr.metrics import compare_test_metrics, load_test_metrics_json, compute_metrics
 
 
-compare_test_metrics(error_on_diff=True)
+test_metrics = load_test_metrics_json()
+
+compare_test_metrics(error_on_diff=False, error_on_miss=False, test_metrics=test_metrics)
+
+compute_metrics(test_metrics=test_metrics)
