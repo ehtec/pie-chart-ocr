3998b1742f1d382bf7412968be43bd2512ae0bc4
g.ulanovas <gabrielius.ulanovas@gmail.com>
2022-01-10 23:31:09 +0100

    update test_helperfunctions.py

---
+++ tests/unit/test_helperfunctions.py
@@ -0,0 +1,10 @@
+import unittest
+from piechartocr import helperfunctions
+import os
+
+
+class TestBaseFunctions(unittest.TestCase):
+
+    def test_rect_from_pre(self):
+        self.assertAlmostEqual()
+        
