bb4f797ee5f283b3fa6f4a3a6a9bc946e373ca16
g.ulanovas <gabrielius.ulanovas@gmail.com>
2022-01-11 13:41:23 +0100

    update test_helperfunctions.py

--- tests/unit/test_helperfunctions.py
+++ tests/unit/test_helperfunctions.py
@@ -1,10 +1,11 @@
 import unittest
 from piechartocr import helperfunctions
-import os
 
 
 class TestBaseFunctions(unittest.TestCase):
 
-    def test_rect_from_pre(self):
-        self.assertAlmostEqual()
-        
+    def test_remove_sc_prefix(self):
+
+        word = helperfunctions.remove_sc_prefix('@;.T3st1n')
+        special_characters = r'[A-Za-z0-9].*'
+        self.assertEqual(word, special_characters)
