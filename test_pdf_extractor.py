c9dfd161a1baac6329c746674462495b7f656f77
Elias Hohl <elias.hohl@ehtec.co>
2022-01-13 22:41:24 +0100

    change loglevel

--- test_pdf_extractor.py
+++ test_pdf_extractor.py
@@ -1,7 +1,7 @@
 import logging
+logging.basicConfig(level=logging.INFO)
 from piechartocr.pdf_extractor import extract_tuples_from_pdf
 
 
-logging.basicConfig(level=logging.WARNING)
 path = "/home/elias/collected-whitepapers/METASEER_Whitepaper_v7.7.pdf"
 extract_tuples_from_pdf(path)
