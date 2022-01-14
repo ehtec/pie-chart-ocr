9f806ae776b8671cfb8e27fd0f47d349e3186e81
Elias Hohl <elias.hohl@ehtec.co>
2022-01-14 02:20:40 +0100

    successful left-to-right sorting test

--- test_pdf_extractor.py
+++ test_pdf_extractor.py
@@ -4,7 +4,10 @@ from piechartocr.pdf_extractor import extract_tuples_from_pdf
 
 
 # path = "/home/elias/collected-whitepapers/Finminity-Brochure(1).pdf"
-path = "/home/elias/collected-whitepapers/METASEER_Whitepaper_v7.7.pdf"
+# path = "/home/elias/collected-whitepapers/METASEER_Whitepaper_v7.7.pdf"
 # path = "/home/elias/collected-whitepapers/7plusWhitePaper_compressed(1).pdf"
+# path = "/home/elias/new-collected-whitepapers/collected_whitepapers/WHITEPAPER.pdf"
+# path = "/home/elias/new-collected-whitepapers/collected_whitepapers/Drife Whitepaper.pdf"
+path = "/home/elias/new-collected-whitepapers/collected_whitepapers/GMS WHITEPAPER (18).pdf"
 
 extract_tuples_from_pdf(path, return_unsorted_output=True, sort_direction='left-to-right')
