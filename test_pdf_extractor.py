4b34ca959c35a8bceb852b29c496df9c17e69db3
Elias Hohl <elias.hohl@ehtec.co>
2022-01-14 02:43:42 +0100

    run another test

--- test_pdf_extractor.py
+++ test_pdf_extractor.py
@@ -6,8 +6,8 @@ from piechartocr.pdf_extractor import extract_tuples_from_pdf
 # path = "/home/elias/collected-whitepapers/Finminity-Brochure(1).pdf"
 # path = "/home/elias/collected-whitepapers/METASEER_Whitepaper_v7.7.pdf"
 # path = "/home/elias/collected-whitepapers/7plusWhitePaper_compressed(1).pdf"
-# path = "/home/elias/new-collected-whitepapers/collected_whitepapers/WHITEPAPER.pdf"
+path = "/home/elias/new-collected-whitepapers/collected_whitepapers/WHITEPAPER.pdf"
 # path = "/home/elias/new-collected-whitepapers/collected_whitepapers/Drife Whitepaper.pdf"
-path = "/home/elias/new-collected-whitepapers/collected_whitepapers/GMS WHITEPAPER (18).pdf"
+# path = "/home/elias/new-collected-whitepapers/collected_whitepapers/GMS WHITEPAPER (18).pdf"
 
-extract_tuples_from_pdf(path, return_unsorted_output=True, sort_direction='left-to-right')
+extract_tuples_from_pdf(path, return_unsorted_output=True, sort_direction='top-to-bottom')
