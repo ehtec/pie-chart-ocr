a20f2efad85042e9f526e7d43cba8cb1a32c119a
Elias Hohl <elias.hohl@ehtec.co>
2022-01-14 01:05:35 +0100

    add more tests

--- test_pdf_extractor.py
+++ test_pdf_extractor.py
@@ -3,5 +3,8 @@ logging.basicConfig(level=logging.INFO)
 from piechartocr.pdf_extractor import extract_tuples_from_pdf
 
 
+# path = "/home/elias/collected-whitepapers/Finminity-Brochure(1).pdf"
 path = "/home/elias/collected-whitepapers/METASEER_Whitepaper_v7.7.pdf"
+# path = "/home/elias/collected-whitepapers/7plusWhitePaper_compressed(1).pdf"
+
 extract_tuples_from_pdf(path, return_unsorted_output=True)
