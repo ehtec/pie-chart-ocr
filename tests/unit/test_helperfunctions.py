322c22ba1c666d8e64adfa0a10d09757719ae997
g.ulanovas <gabrielius.ulanovas@gmail.com>
2022-01-14 01:49:00 +0100

    update test_helperfunctions.py

--- tests/unit/test_helperfunctions.py
+++ tests/unit/test_helperfunctions.py
@@ -6,6 +6,12 @@ class TestHelperFunctions(unittest.TestCase):
 
     def test_remove_sc_prefix(self):
 
-        word = helperfunctions.remove_sc_prefix('@;.T3st1n')
-        word1 = 'T3st1n'
+        word = helperfunctions.remove_sc_prefix('@Testing')
+        word1 = 'Testing'
+        self.assertEqual(word, word1)
+
+    def test_remove_sc_suffix(self):
+
+        word = helperfunctions.remove_sc_suffix('Testing@')
+        word1 = 'Testing'
         self.assertEqual(word, word1)
