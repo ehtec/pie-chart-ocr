c1b3e3b8484aff178917831c1e3c11238b30ecc3
g.ulanovas <gabrielius.ulanovas@gmail.com>
2022-01-16 21:51:06 +0100

    update test_helperfunctions.py test_integerize function

--- tests/unit/test_helperfunctions.py
+++ tests/unit/test_helperfunctions.py
@@ -34,3 +34,6 @@ class TestHelperFunctions(unittest.TestCase):
         list1 = helperfunctions.integerize([2.0, 3.0])
         list2 = ([2, 3])
         self.assertAlmostEqual(list1, list2)
+        i = []
+        for i in list1:
+            self.assertIsInstance(i, int)
