b5752a64990fa5fd2f324fd5ad5217f0de4b867f
Elias Hohl <elias.hohl@ehtec.co>
2022-01-17 01:12:31 +0100

    resolve conflict

--- tests/unit/test_helperfunctions.py
+++ tests/unit/test_helperfunctions.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from piechartocr import helperfunctions
 
 
@@ -15,3 +16,16 @@ class TestHelperFunctions(unittest.TestCase):
         word = helperfunctions.remove_sc_suffix('Testing@')
         word1 = 'Testing'
         self.assertEqual(word, word1)
+
+    def test_rect_from_pre(self):
+
+        p1 = helperfunctions.rect_from_pre((0, 2, 1, 0))
+        p2 = ((0, 2), (0, 0), (1, 0), (1, 2))
+        self.assertEqual(p1, p2)
+
+    def test_hash_file(self):
+
+        path = helperfunctions.get_root_path()
+
+        file_path = helperfunctions.hash_file(os.path.join(path, 'tox.ini'))
+        self.assertFalse(os.path.isfile(file_path))
