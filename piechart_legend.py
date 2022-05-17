72e1dd07ff1381ceebaf37938bb1306d87948e9a
Mohan Borra <mohanborra00783@gmail.com>
2022-05-17 23:37:09 +0530

    added limit to the area of the sector

--- piechart_legend.py
+++ piechart_legend.py
@@ -9,6 +9,14 @@ plt.title(label="Society Food Preference",
           fontstyle='italic')
 
 
+def numbers_sum(n, m):
+    sizes = np.random.dirichlet(np.ones(n)) * m
+    for size in sizes:
+        if size < 3.0:
+            sizes = numbers_sum(n, m)
+    return sizes
+
+
 def pie_chart_generator_legend(labels, data, legend=True):
     fig = plt.figure(4, figsize=(10, 10))
     plt.rcParams['font.size'] = 18.0
@@ -37,7 +45,6 @@ for _ in range(10):
     labels_1 = [word_list[randint(1, 100000)] for i in range(5)]
     n = 5
     m = 100
-    sizes = np.random.dirichlet(np.ones(n)) * m
+    sizes = numbers_sum(n, m)
     labels = [f'{l}, {s:0.1f}%' for l, s in zip(labels_1, sizes)]
     pie_chart_generator_legend(labels=labels, data=sizes)
-    # pie_chart_generator_legend(labels=labels, data=sizes, legend=False)
--- piechart_without_legend.py
+++ piechart_without_legend.py
@@ -3,7 +3,14 @@ from random import randint
 from nltk.corpus import words
 import numpy as np
 import uuid
-from matplotlib import font_manager as fm
+
+
+def numbers_sum(n, m):
+    sizes = np.random.dirichlet(np.ones(n)) * m
+    for size in sizes:
+        if size < 3.0:
+            sizes = numbers_sum(n, m)
+    return sizes
 
 
 def fix_labels(mylabels, tooclose=0.1, sepfactor=2):
@@ -27,19 +34,14 @@ for _ in range(10):
     ax1.axis('equal')
     n = 5
     m = 100
-    sizes = np.random.dirichlet(np.ones(n)) * m
+    sizes = numbers_sum(n, m)
     print(sizes)
     word_list = words.words()
     groups = [word_list[randint(1, 100000)] for i in range(5)]
     print(groups)
     filename = uuid.uuid4().hex
-    explode = [0.1, 0.1, 0.1, 0.1, 0.1]
-    wedges, labels, autopct = ax1.pie(sizes, explode=explode, labels=groups, autopct='%1.1f%%',
+    wedges, labels, autopct = ax1.pie(sizes, labels=groups, autopct='%1.1f%%',
                                       shadow=False, startangle=90)
-    proptease = fm.FontProperties()
-    proptease.set_size('xx-small')
-    #plt.setp(labels, fontproperties=proptease)
-    plt.setp(autopct, fontproperties=proptease)
     fix_labels(autopct, sepfactor=3)
     fix_labels(labels, sepfactor=2)
     plt.tight_layout()
