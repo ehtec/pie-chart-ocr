525b82123ae988bce3af364c5e09b0691bec9460
Elias Hohl <elias.hohl@ehtec.co>
2022-08-17 23:24:50 +0200

    add annotations

--- piechartocr/generate_test_data.py
+++ piechartocr/generate_test_data.py
@@ -2,9 +2,10 @@ import random
 from matplotlib import pyplot as plt
 import numpy as np
 from nltk.corpus import words
-import uuid
+# import uuid
 from .helperfunctions import clean_folder_contents, get_root_path
 import os
+from . import data_helpers
 
 
 # minimum word count in a caption
@@ -60,7 +61,7 @@ def split_number(count, total_value, min_size=0.0):
 
 
 # generate pie chart from provided data
-def pie_chart_generator(labels, data, legend=True):
+def pie_chart_generator(labels, data, chart_index, legend=True):
 
     if legend:
         fig = plt.figure(4, figsize=(12, 13))
@@ -74,7 +75,29 @@ def pie_chart_generator(labels, data, legend=True):
     ax.axis("equal")
     # ax2 = fig.add_subplot(212)
     # ax2.axis("off")
-    filename = uuid.uuid4().hex
+    # filename = uuid.uuid4().hex
+
+    foldername = f"Chart_{chart_index + 1}"
+
+    # print("data: {0}".format(data))
+    # print("labels: {0}".format(labels))
+
+    annotations = [(label.replace('\n', ' '), value / 100) for label, value in zip(labels, data)]
+    # print("annotations: {0}".format(annotations))
+
+    if legend:
+        folder_path = os.path.join(DATA_ROOT, 'generated_pie_charts_legend', foldername)
+    else:
+        folder_path = os.path.join(DATA_ROOT, 'generated_pie_charts_without_legend', foldername)
+
+    if os.path.isdir(folder_path):
+        clean_folder_contents(folder_path)
+    else:
+        os.mkdir(folder_path)
+
+    csvpath = os.path.join(folder_path, 'annotation.csv')
+
+    data_helpers.write_annotations_to_csv(csvpath, annotations)
 
     if legend:
         ax2 = fig.add_subplot(212)
@@ -82,20 +105,20 @@ def pie_chart_generator(labels, data, legend=True):
         pie = ax.pie(data, startangle=90, autopct='%1.1f%%', pctdistance=1.3)
         plt.legend(pie[0], labels, labelspacing=1.0, loc="upper left")
         plt.tight_layout()
-        output_path = os.path.join(DATA_ROOT, 'generated_pie_charts_legend', filename + '.png')
+        output_path = os.path.join(folder_path, 'image.png')
         plt.savefig(output_path)
         plt.close()
 
     else:
         ax.pie(data, startangle=90, autopct='%1.1f%%', labels=labels, pctdistance=0.7, labeldistance=1.5, radius=0.5)
         plt.tight_layout()
-        output_path = os.path.join(DATA_ROOT, 'generated_pie_charts_without_legend', filename + '.png')
+        output_path = os.path.join(folder_path, 'image.png')
         plt.savefig(output_path)
         plt.close()
 
 
 # generate a random pie chart with only the number of sectors given
-def generate_random_pie_chart(num_sectors, legend=True):
+def generate_random_pie_chart(num_sectors, chart_index, legend=True):
 
     word_list = words.words()[100000:]
     all_label_list = []
@@ -108,7 +131,7 @@ def generate_random_pie_chart(num_sectors, legend=True):
     sum_number = 100.0
     areas = split_number(num_sectors, sum_number, MIN_SECTOR_PERCENTAGE)
     # data_label = [f'{label}' for label, size in zip(all_label_list, areas)]
-    pie_chart_generator(labels=all_label_list, data=areas, legend=legend)
+    pie_chart_generator(labels=all_label_list, data=areas, chart_index=chart_index, legend=legend)
 
 
 def main():
@@ -126,8 +149,8 @@ def main():
         clean_folder_contents(path)
 
     for counter in range(TOTAL_CHARTS_COUNT):
-        generate_random_pie_chart(random.randint(MIN_SECTOR_COUNT, MAX_SECTOR_COUNT), legend=False)
-        generate_random_pie_chart(random.randint(MIN_SECTOR_COUNT, MAX_SECTOR_COUNT), legend=True)
+        generate_random_pie_chart(random.randint(MIN_SECTOR_COUNT, MAX_SECTOR_COUNT), counter, legend=False)
+        generate_random_pie_chart(random.randint(MIN_SECTOR_COUNT, MAX_SECTOR_COUNT), counter, legend=True)
 
 
 if __name__ == "__main__":
