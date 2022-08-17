7ca253330210297eb14cbdf2bfed718c8262b0d8
Elias Hohl <elias.hohl@ehtec.co>
2022-08-17 22:51:32 +0200

    major refactoring of feature

--- piechartocr/generate_test_data.py
+++ piechartocr/generate_test_data.py
@@ -3,23 +3,30 @@ from matplotlib import pyplot as plt
 import numpy as np
 from nltk.corpus import words
 import uuid
+from .helperfunctions import clean_folder_contents, get_root_path
 import os
-import shutil
-import logging
 
 
-def clean_folder_contents(folder):
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            logging.exception(e)
+# minimum word count in a caption
+MIN_WORD_COUNT = 1
+
+# maximum word count in a caption
+MAX_WORD_COUNT = 5
+
+# minimum sector count
+MIN_SECTOR_COUNT = 3
+
+# maximum sector count
+MAX_SECTOR_COUNT = 8
+
+# minimum sector percentage
+MIN_SECTOR_PERCENTAGE = 4.0
+
+# total count of charts generated
+TOTAL_CHARTS_COUNT = 15
 
 
+# add linebreaks to words that will be used as pie chart captions
 def linebreak_words(labels):
 
     if len(labels) < 3:
@@ -34,6 +41,7 @@ def linebreak_words(labels):
     return ValueError("len(labels) is too large")
 
 
+# split total_value into count different values, each of them is at least min_size
 def split_number(count, total_value, min_size=0.0):
 
     if total_value < count * min_size:
@@ -48,12 +56,15 @@ def split_number(count, total_value, min_size=0.0):
     return res_sizes
 
 
-def pie_chart_generator_legend(labels, data, legend=True):
+# generate pie chart from provided data
+def pie_chart_generator(labels, data, legend=True):
+
     if legend:
-        fig = plt.figure(4, figsize=(12, 12))
+        fig = plt.figure(4, figsize=(12, 13))
         ax = fig.add_subplot(211)
     else:
-        fig, ax = plt.subplots(figsize=(20, 12))
+        fig, ax = plt.subplots(figsize=(20, 13))
+
     plt.rcParams['font.size'] = 20.0
     # ax = fig.add_subplot(211)
     # ax.set_title('Random title')
@@ -61,46 +72,51 @@ def pie_chart_generator_legend(labels, data, legend=True):
     # ax2 = fig.add_subplot(212)
     # ax2.axis("off")
     filename = uuid.uuid4().hex
+
     if legend:
         ax2 = fig.add_subplot(212)
         ax2.axis("off")
         pie = ax.pie(data, startangle=90, autopct='%1.1f%%', pctdistance=1.3)
         plt.legend(pie[0], labels, labelspacing=1.0, loc="upper left")
         plt.tight_layout()
-        plt.savefig("generated_pie_charts/generated_pie_charts_legend/" + filename[:6] + "_legend.png")
+        plt.savefig("generated_pie_charts/generated_pie_charts_legend/" + filename + ".png")
         plt.close()
+
     else:
         ax.pie(data, startangle=90, autopct='%1.1f%%', labels=labels, pctdistance=0.7, labeldistance=1.5, radius=0.5)
         plt.tight_layout()
-        plt.savefig("generated_pie_charts/generated_pie_charts_without_legend/" + filename[:6] + ".png")
+        plt.savefig("generated_pie_charts/generated_pie_charts_without_legend/" + filename + ".png")
         plt.close()
 
 
-def num_of_piecharts(num_sectors, legend=True):
+# generate a random pie chart with only the number of sectors given
+def generate_random_pie_chart(num_sectors, legend=True):
+
     word_list = words.words()[100000:]
     all_label_list = []
+
     for i in range(num_sectors):
-        label_list = [random.choice(word_list) for j in range(random.randint(1, 5))]
+        label_list = [random.choice(word_list) for _ in range(random.randint(MIN_WORD_COUNT, MAX_WORD_COUNT))]
         label_string = linebreak_words(label_list)
         all_label_list.append(label_string)
 
     sum_number = 100.0
-    areas = split_number(num_sectors, sum_number, 4.0)
-    data_label = [f'{label}' for label, size in zip(all_label_list, areas)]
-    pie_chart_generator_legend(labels=data_label, data=areas, legend=legend)
+    areas = split_number(num_sectors, sum_number, MIN_SECTOR_PERCENTAGE)
+    # data_label = [f'{label}' for label, size in zip(all_label_list, areas)]
+    pie_chart_generator(labels=all_label_list, data=areas, legend=legend)
 
-
-# clean_folder_contents("generated_pie_charts/generated_pie_charts_without_legend/")
-# clean_folder_contents("generated_pie_charts/generated_pie_charts_legend/")
 
 def main():
-    path = "generated_pie_charts/generated_pie_charts_without_legend/"
+
+    path = os.path.join(get_root_path(), "generated_pie_charts", "generated_pie_charts_without_legend")
     clean_folder_contents(path)
-    path = "generated_pie_charts/generated_pie_charts_legend/"
+
+    path = os.path.join(get_root_path(), "generated_pie_charts", "generated_pie_charts_legend")
     clean_folder_contents(path)
-    count = 15
-    for counter in range(count):
-        num_of_piecharts(random.randint(3, 8), legend=False)
+
+    for counter in range(TOTAL_CHARTS_COUNT):
+        generate_random_pie_chart(random.randint(MIN_SECTOR_COUNT, MAX_SECTOR_COUNT), legend=False)
+        generate_random_pie_chart(random.randint(MIN_SECTOR_COUNT, MAX_SECTOR_COUNT), legend=True)
 
 
 if __name__ == "__main__":
---
+++ run_generate_test_data.py
@@ -0,0 +1,5 @@
+from piechartocr import generate_test_data
+
+
+if __name__ == "__main__":
+    generate_test_data.main()
